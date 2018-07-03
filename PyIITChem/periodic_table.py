b788dac08c91f16a8718998d14660a39ca71403c
Naimish <naimish240@gmail.com>
2018-07-03 17:38:55 +0530

    Update

--- PyIITChem/periodic_table.py
+++ PyIITChem/periodic_table.py
@@ -25,65 +25,6 @@ def sorted_by_atomic_number(arr):
 
 '''___________________________________________________________________________________________________________________'''
 
-# Function to return Electronic Configuraton of an element
-# Takes atomic number as input, returns string
-
-def atomic_config(num): # num = atomic number = number of electrons (if neutral)
-    # s orbital : 2 electrons
-    # p orbital : 6 electrons
-    # d orbital : 10 electrons
-    # f orbital : 14 electrons
-    
-    order=['1s','2s','2p','3s','3p','4s','3d','4p','5s','4d','5p','6s','4f','5d','6p','7s','5f','6d','7p','8s','6f','7d','8p','9s']
-    
-    # Exceptions (atomic, but valid for configurations too):
-    if num == 24: # Chromium 
-        return '1s2 2s2 2p6 3s2 3p6 4s1 3d5'
-    elif num == 29: # Copper
-        return '1s2 2s2 2p6 3s2 3p6 4s1 3d10'
-    elif num == 41: # Niobium
-        return '1s2 2s2 2p6 3s2 3p6 4s2 3d10 4p6 5s1 4d4'
-    elif num == 42: # Molybdenum
-        return '1s2 2s2 2p6 3s2 3p6 4s2 3d10 4p6 5s1 4d5'
-    elif num == 44: # Ruthenium
-        return '1s2 2s2 2p6 3s2 3p6 4s2 3d10 4p6 5s1 4d7'
-    elif num == 45: # Rodhium
-        return '1s2 2s2 2p6 3s2 3p6 4s2 3d10 4p6 5s1 4d8'
-    elif num == 46: # Palladium
-        return '1s2 2s2 2p6 3s2 3p6 4s2 3d10 4p6 4d10'
-    elif num == 47: # Silver
-        return '1s2 2s2 2p6 3s2 3p6 4s2 3d10 4p6 5s1 4d10'
-    elif num == 78: # Platinum
-        return '1s2 2s2 2p6 3s2 3p6 4s2 3d10 4p6 4d10 5s2 5p6 6s1 4f14 5d9'
-    elif num == 79: # Gold
-        return '1s2 2s2 2p6 3s2 3p6 4s2 3d10 4p6 4d10 5s2 5p6 6s1 4f14 5d10'
-    
-    config=''
-    i=0
-    # While loop for others
-    while num>0: 
-        ele=0
-        if 's' in order[i]: # s orbital
-            ele=2
-        elif 'p' in order[i]: # p orbital
-            ele=6
-        elif 'd' in order[i]: # d orbital
-            ele=10
-        else: # f orbital
-            ele=14
-        
-        if num-ele>0:
-            config+=order[i]+str(ele)+' '
-            num-=ele # reduce the electrons from the total number
-            i+=1 # increases i by one
-            continue
-        else:
-            config+=order[i]+str(num)+' '
-            break
-    return config
-
-'''___________________________________________________________________________________________________________________'''
-
 # For first 109 elements along with proton, neutron and electron at end
 
 periodic_table = [
